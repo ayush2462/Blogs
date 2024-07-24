88c689c0ad49c6aebed14bb080d71a1da220ad5f
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-07-24 17:40:15 +0530

    "Added Login, Register, and CreatePost routes to App.js"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CreatePost from "./pages/CreatePost";
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/write" element={<CreatePost />} />
       </Routes>
     </>
   );
