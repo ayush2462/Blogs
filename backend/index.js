a6bc161530a8855d8399e1ab7197aef92d5bb0f0
Ayush Vishwakarma <128534675+ayush2462@users.noreply.github.com>
2024-07-25 02:24:07 +0530

    "Added dotenv config, express.json middleware, and implemented database connection and server startup logic."

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const multer = require("multer");
 const app = express();
+dotenv.config();
+app.use(express.json())
 app.use(cors());
 const corsOptions = {
   origin: "*",
@@ -11,7 +13,16 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-
-const connectDB= async()=>{
-    
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connection established successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  connectDB();
+});
